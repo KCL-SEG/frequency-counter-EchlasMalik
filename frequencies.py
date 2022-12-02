478c755f8f72d8c16d1d328704d56c950787b429
Echlas Malik <99184338+EchlasMalik@users.noreply.github.com>
2022-12-02 00:32:20 +0000

    All tests passed

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,15 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    strItems = []
     frequencies = {}
-    # Your code goes here
+    for item in items:  #Convert all items to strings
+        converted_key = str(item)
+        strItems.append(converted_key)
+
+    for strItem in strItems:    #Check membership and then update the value in hashmap.
+        if strItem in frequencies:
+            frequencies[strItem] = frequencies.get(strItem, 0) + 1
+        else:
+            frequencies[strItem] = 1
     return frequencies
